43da3bcbad89210cbe4e0792bf46119f90137aea
Timon van Spronsen <timonvanspronsen@outlook.com>
2016-08-15 13:24:45 +0200

    Add support for CSS modules

--- src/config/webpack.styles.js
+++ src/config/webpack.styles.js
@@ -2,8 +2,10 @@ import autoprefixer from 'autoprefixer'
 import inlineSvg from 'postcss-inline-svg'
 import { SRC_DIR } from './helpers'
 
-const scssTest = /\.scss$/
-const cssTest = /\.css$/
+const cssTest = /^((?!\.module).)*css$/
+const scssTest = /^((?!\.module).)*scss$/
+const cssModuleTest = /\.module.css$/
+const scssModuleTest = /\.module.scss$/
 
 function createConfig (conf) {
   const config = {
@@ -18,6 +20,16 @@ function createConfig (conf) {
           test: scssTest,
           include: SRC_DIR,
           loaders: ['style', 'css', 'postcss', 'sass']
+        },
+        {
+          test: cssModuleTest,
+          include: SRC_DIR,
+          loaders: ['style', 'css?modules', 'postcss']
+        },
+        {
+          test: scssModuleTest,
+          include: SRC_DIR,
+          loaders: ['style', 'css?modules', 'postcss', 'sass']
         }
       ]
     },

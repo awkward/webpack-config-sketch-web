9abcd02b67d2fa49667839bda8b1b767e4457185
Timon van Spronsen <timonvanspronsen@outlook.com>
2016-08-16 16:22:36 +0200

    Use history api fallback for HMR

--- src/bin/dev-server.js
+++ src/bin/dev-server.js
@@ -1,14 +1,18 @@
-import fs from 'fs'
 import path from 'path'
 import express from 'express'
 import webpack from 'webpack'
 import WebpackDevMiddleware from 'webpack-dev-middleware'
 import WebpackHotMiddleware from 'webpack-hot-middleware'
+import connectHistoryApi from 'connect-history-api-fallback'
 import { ROOT } from '../helpers'
 
 const webpackConfig = require(path.join(ROOT, 'webpack.config.js'))
 const app = express()
 const compiler = webpack(webpackConfig)
+
+app.use(connectHistoryApi({
+  verbose: false
+}))
 
 app.use(WebpackDevMiddleware(compiler, {
   noInfo: true,
@@ -19,17 +23,6 @@ app.use(WebpackDevMiddleware(compiler, {
 app.use(WebpackHotMiddleware(compiler))
 
 app.use('/public', express.static('public'))
-
-app.get('*', function (req, res) {
-  fs.readFile(path.join(compiler.outputPath, 'index.html'), (err, file) => {
-    // if (err) {
-    //   res.sendStatus(404)
-    // } else {
-    //   res.send(file.toString())
-    // }
-    res.send(file.toString())
-  })
-})
 
 app.listen(3000, function (err) {
   if (err) {

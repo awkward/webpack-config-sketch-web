03bc6078cf4c552749229d97e192635627b025ef
Timon van Spronsen <timonvanspronsen@outlook.com>
2016-08-17 14:08:18 +0200

    Fix: SVG loader configuration

--- src/config/webpack.assets.js
+++ src/config/webpack.assets.js
@@ -1,16 +1,27 @@
+const svgTest = /^((?!\.inline).)*\.svg$/i
+const imagesTest = /.*\.(gif|png|jpe?g)$/i
+const inlineSvgTest = /\.inline\.svg/i
+
 function createConfig (conf) {
   const config = {
     module: {
       loaders: [
         {
-          test: /.*\.(gif|png|jpe?g|svg)$/i,
+          test: imagesTest,
           loaders: [
             'file?hash=sha512&digest=hex&name=[hash].[ext]',
             'image-webpack'
           ]
         },
         {
-          test: /\.inline\.svg/i,
+          test: svgTest,
+          loaders: [
+            'file?hash=sha512&digest=hex&name=[hash].[ext]',
+            'image-webpack'
+          ]
+        },
+        {
+          test: inlineSvgTest,
           loaders: [
             'svg-inline?classPrefix',
             'image-webpack'
